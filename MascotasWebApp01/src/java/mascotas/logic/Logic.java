24f8832d045373d85ced97c19f758725c281c2f3
RobertoJ6 <robertojose-16@hotmail.com>
2019-04-19 12:44:14 -0600

    Fix

--- MascotasWebApp01/src/java/mascotas/logic/Logic.java
+++ MascotasWebApp01/src/java/mascotas/logic/Logic.java
@@ -41,15 +41,4 @@ public class Logic
         int iRows = database.executeNonQueryRows(strSql);
         return iRows;
     }    
-    }    
-     public int deleteTableRows(int p_iId,String p_strTableName) 
-    {
-        //delete from travelsys.client where id=0;
-        DatabaseX database = getDatabase();
-        String strSql = "delete from mascotasdb." +p_strTableName+ " "
-                + " where id =" +p_iId+" ";
-        System.out.println(strSql);
-        int iRows = database.executeNonQueryRows(strSql);
-        return iRows;
-    }
 }
